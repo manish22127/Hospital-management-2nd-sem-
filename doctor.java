ac44f7cfa91152040be099c6991d96f6b8c480df
manish22127 <manish.22127@gmail.com>
2021-08-26 12:28:54 +0530

    doctor added

---
+++ doctor.java
@@ -0,0 +1,92 @@
+package gp3;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.Statement;
+import java.util.Scanner;
+
+public class doctor {
+	Scanner s = new Scanner(System.in);
+	public String d_name,speaciality;
+	
+	public void add_doctor()
+	{
+		System.out.println("enter the DOCTOR name ");
+		d_name=s.next();
+		System.out.println("enter the Doctor Speaciality");
+		speaciality=s.next();
+		
+		
+		Connection con = null;
+		try {
+			Class.forName("com.mysql.cj.jdbc.Driver");
+			con = DriverManager.getConnection("jdbc:mysql://localhost:3306/hospital_group3","manish","Manish@123");
+			System.out.println("Connection created...");
+			
+			Statement st = con.createStatement();
+			String qry = "insert into doctor values('"+d_name+"','"+speaciality+"');";
+			st.execute(qry);
+			System.out.println("*********************************");
+			System.out.println("Record inserted...");
+			System.out.println("*********************************");
+			con.close();
+		}
+				
+		
+		catch(Exception e) {
+			e.printStackTrace();
+		}
+		
+	}
+	
+	public void remove_doctor() {
+		String temp;
+		System.out.println("enter the doctor name to delete");
+		temp=s.next();
+		
+		Connection con = null;
+		try {
+			Class.forName("com.mysql.cj.jdbc.Driver");
+			con = DriverManager.getConnection("jdbc:mysql://localhost:3306/hospital_group3","manish","Manish@123");
+			System.out.println("Connection created...");
+			
+			Statement st = con.createStatement();
+			String qry = "delete from doctor where name='"+temp+"';";
+			st.executeUpdate(qry);
+			System.out.println("*********************************");
+			System.out.println("Record deleted...");
+			System.out.println("*********************************");
+			con.close();
+		}
+		catch(Exception e) {
+			e.printStackTrace();
+		}
+	}
+	
+	
+		public static void main(String[] args) {
+			Scanner s = new Scanner(System.in);
+			doctor d = new doctor();
+			int i=1;
+			System.out.println("WELCOME TO DOCTOR PORTAL");
+			
+			while(i!=0) {
+			System.out.println("1.ADD DOCTOR \n2.REMOVE DOCTOR \n3.EXIT FROM DOCTOR MODULE");
+			int ch = s.nextInt();
+			switch(ch) {
+			case 1:
+				d.add_doctor();
+				break;
+			case 2:
+				d.remove_doctor();
+				break;
+			case 3:
+				System.out.println("THANKYOU");
+				i=0;
+				break;
+			default:
+				System.out.println("enter valid details \n");
+
+			}}
+		}
+}
--- info.java
+++ info.java
@@ -1,7 +1,51 @@
 package gp3;
 
+import java.util.Scanner;
+
 public class info {
 		public static void main(String[] args) {
-			patient.main(null);
+			Scanner scan = new Scanner(System.in);
+			
+			int i=0;
+			while (i==0){
+			System.out.println("1.Patients \n2.Surgical Waiting list \n3.Add Diagnostic Test \n4.Add Referal \n 5.Surgeon\n");
+			int choice = scan.nextInt();
+			switch(choice) {
+			case 1:
+			{
+				System.out.println("Entering Patients Module");
+				patient.main(null);
+			}
+			case 2:
+			{
+				System.out.println("Entering Surgicals Module");
+			}
+			case 3:
+			{
+				System.out.println("Entering Diagnostic test Module");
+			}
+			case 4:
+			{
+				System.out.println("Entering Referral Module");
+			}
+			case 5:
+			{
+				System.out.println("Entering Doctor Module");
+			}
+			case 6:
+			{
+				System.out.println("Exitting.....");
+				i=1;
+			}
+			default:
+			{
+				System.out.println("Please enter proper data");
+			}
+			}
+			
+			
+			
+			}
+			
 		}
 }
